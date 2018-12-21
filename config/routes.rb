0d20320018dc91115dfaf778abdf5109c00a5dd2
Greg Morgan <gregm9999@gmail.com>
2018-12-21 21:49:10 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
   get '/register', to: 'users#new', as: 'register'
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 7a537ae4d18ad8c7e0e9aa9c7e060ef1ebfea3bd
